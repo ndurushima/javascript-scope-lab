32875528c4f994e743e554c8b7f7055a38a94bc0
ndurushima <ndu1994@gmail.com>
2025-06-10 15:46:36 -0500

    Javascript Scope Lab FlatBurger

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Write your solution in this file!
+let burgers = ['Hamburger', 'Cheeseburger'];
+let featuredDrink = 'Straberry Milkshake';
+
+function addBurger() {
+    let newBurger = 'Flatburger';
+    burgers.push(newBurger);
+}
+
+if(true) {
+    let anotherNewBurger = 'Maple Bacon Burger';
+    burgers.push(anotherNewBurger);
+}
+
+function changeFeaturedDrink() {
+    featuredDrink = 'The JavaShake';
+}
+
+addBurger();
+console.log(burgers);
+
+changeFeaturedDrink();
+console.log(featuredDrink);
